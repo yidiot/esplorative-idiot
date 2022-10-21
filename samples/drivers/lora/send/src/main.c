91849c5c001de8e41956349687c79d0b2d93a37e
Gaël PORTAY <gael.portay@gmail.com>
2022-10-21 13:48:11 +0200

    Enable FrameBuffer

--- samples/drivers/lora/send/src/main.c
+++ samples/drivers/lora/send/src/main.c
@@ -1,14 +1,18 @@
 /*
- * Copyright (c) 2019 Manivannan Sadhasivam
+ * Copyright (c) 2022 Gaël PORTAY
+ *               2019 Manivannan Sadhasivam
+ *               2018 PHYTEC Messtechnik GmbH
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
 #include <zephyr/device.h>
 #include <zephyr/drivers/lora.h>
+#include <zephyr/display/cfb.h>
 #include <errno.h>
 #include <zephyr/sys/util.h>
 #include <zephyr/kernel.h>
+#include <stdio.h>
 
 #define DEFAULT_RADIO_NODE DT_ALIAS(lora0)
 BUILD_ASSERT(DT_NODE_HAS_STATUS(DEFAULT_RADIO_NODE, okay),
@@ -25,8 +29,54 @@ char data[MAX_DATA_LEN] = {'h', 'e', 'l', 'l', 'o', 'w', 'o', 'r', 'l', 'd'};
 void main(void)
 {
 	const struct device *const lora_dev = DEVICE_DT_GET(DEFAULT_RADIO_NODE);
+	const struct device *dev;
 	struct lora_modem_config config;
+	uint16_t rows;
+	uint8_t ppt;
+	uint8_t font_width;
+	uint8_t font_height;
 	int ret;
+
+	dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
+	if (!device_is_ready(dev)) {
+		printf("Device %s not ready\n", dev->name);
+		return;
+	}
+
+	if (display_set_pixel_format(dev, PIXEL_FORMAT_MONO10) != 0) {
+		printf("Failed to set required pixel format\n");
+		return;
+	}
+
+	printf("Initialized %s\n", dev->name);
+
+	if (cfb_framebuffer_init(dev)) {
+		printf("Framebuffer initialization failed!\n");
+		return;
+	}
+
+	cfb_framebuffer_clear(dev, true);
+
+	display_blanking_off(dev);
+
+	rows = cfb_get_display_parameter(dev, CFB_DISPLAY_ROWS);
+	ppt = cfb_get_display_parameter(dev, CFB_DISPLAY_PPT);
+
+	for (int idx = 0; idx < 42; idx++) {
+		if (cfb_get_font_size(dev, idx, &font_width, &font_height)) {
+			break;
+		}
+		cfb_framebuffer_set_font(dev, idx);
+		printf("font width %d, font height %d\n",
+		       font_width, font_height);
+	}
+
+	printf("x_res %d, y_res %d, ppt %d, rows %d, cols %d\n",
+	       cfb_get_display_parameter(dev, CFB_DISPLAY_WIDTH),
+	       cfb_get_display_parameter(dev, CFB_DISPLAY_HEIGH),
+	       ppt,
+	       rows,
+	       cfb_get_display_parameter(dev, CFB_DISPLAY_COLS));
 
 	if (!device_is_ready(lora_dev)) {
 		LOG_ERR("%s Device not ready", lora_dev->name);
@@ -57,6 +107,25 @@ void main(void)
 		}
 
 		LOG_INF("Data sent!");
+
+		if (dev) {
+			static int i = 0;
+			char buf[10+1]; /* INT_MAX + NUL */
+
+			cfb_framebuffer_clear(dev, false);
+			if (cfb_print(dev, "TX:", 0, 0)) {
+				LOG_ERR("Failed to print a string\n");
+				continue;
+			}
+
+			sprintf(buf, "%d", ++i);
+			if (cfb_print(dev, buf, 0, 1 * font_height)) {
+				LOG_ERR("Failed to print a string\n");
+				continue;
+			}
+
+			cfb_framebuffer_finalize(dev);
+		}
 
 		/* Send data at 1s interval */
 		k_sleep(K_MSEC(1000));
