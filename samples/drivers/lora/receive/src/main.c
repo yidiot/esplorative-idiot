08dfa7e408400b169b65f7237130fcce2b809d2a
Gaël PORTAY <gael.portay@gmail.com>
2022-10-21 20:08:41 +0200

    Remove asynchronous receive

--- samples/drivers/lora/receive/src/main.c
+++ samples/drivers/lora/receive/src/main.c
@@ -20,24 +20,6 @@ BUILD_ASSERT(DT_NODE_HAS_STATUS(DEFAULT_RADIO_NODE, okay),
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(lora_receive);
 
-void lora_receive_cb(const struct device *dev, uint8_t *data, uint16_t size,
-		     int16_t rssi, int8_t snr)
-{
-	static int cnt;
-
-	ARG_UNUSED(dev);
-	ARG_UNUSED(size);
-
-	LOG_INF("Received data: %s (RSSI:%ddBm, SNR:%ddBm)",
-		data, rssi, snr);
-
-	/* Stop receiving after 10 packets */
-	if (++cnt == 10) {
-		LOG_INF("Stopping packet receptions");
-		lora_recv_async(dev, NULL);
-	}
-}
-
 void main(void)
 {
 	const struct device *const lora_dev = DEVICE_DT_GET(DEFAULT_RADIO_NODE);
@@ -68,10 +50,7 @@ void main(void)
 		return;
 	}
 
-	/* Receive 4 packets synchronously */
-	LOG_INF("Synchronous reception");
-	for (int i = 0; i < 4; i++) {
-		/* Block until data arrives */
+	while (1) {
 		len = lora_recv(lora_dev, data, MAX_DATA_LEN, K_FOREVER,
 				&rssi, &snr);
 		if (len < 0) {
@@ -82,9 +61,4 @@ void main(void)
 		LOG_INF("Received data: %s (RSSI:%ddBm, SNR:%ddBm)",
 			data, rssi, snr);
 	}
-
-	/* Enable asynchronous reception */
-	LOG_INF("Asynchronous reception");
-	lora_recv_async(lora_dev, lora_receive_cb);
-	k_sleep(K_FOREVER);
 }
