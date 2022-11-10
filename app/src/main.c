af6222ebbf1bd908f2533a2c46b5222756f433f0
Gaël PORTAY <gael.portay@gmail.com>
2022-11-10 10:06:31 +0100

    Enable LCD

--- app/src/main.c
+++ app/src/main.c
@@ -4,9 +4,11 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <stdio.h>
 #include <zephyr/kernel.h>
 #include <zephyr/sys/notify.h>
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/display/cfb.h>
 
 struct k_poll_signal signal;
 
@@ -18,9 +20,36 @@ static const struct gpio_dt_spec sw = GPIO_DT_SPEC_GET_OR(DT_ALIAS(sw0),
 							  {0});
 static struct gpio_callback cb;
 
+static const struct device *lcd = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
+
+void handler(struct k_work *work)
+{
+	static int i = 0;
+	char buf[10+1]; /* INT_MAX + NUL */
+	int ret;
+
+	k_poll_signal_raise(&signal, k_uptime_get_32());
+
+	sprintf(buf, "%d", ++i);
+	ret = cfb_print(lcd, buf, 0, 1 * 16);
+	if (ret != 0) {
+		printk("Failed to print string on %s: %d\n", lcd->name, ret);
+		return;
+	}
+
+	ret = cfb_framebuffer_finalize(lcd);
+	if (ret != 0) {
+		printk("Failed to finalize framebuffer on %s: %d\n", lcd->name,
+		       ret);
+		return;
+	}
+}
+
+K_WORK_DEFINE(worker, handler);
+
 void pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
 {
-	k_poll_signal_raise(&signal, k_uptime_get_32());
+	k_work_submit(&worker);
 }
 
 void main(void)
@@ -60,6 +89,59 @@ void main(void)
 	if (ret != 0) {
 		printk("Failed to add callback on %s: %d\n", sw.port->name,
 		       ret);
+		return;
+	}
+
+	if (!device_is_ready(lcd)) {
+		printk("Error: %s is not ready\n", lcd->name);
+		return;
+	}
+
+	ret = display_set_pixel_format(lcd, PIXEL_FORMAT_MONO10);
+	if (ret != 0) {
+		printk("Failed to set pixel format on %s: %d\n", lcd->name,
+		       ret);
+		return;
+	}
+
+	ret = cfb_framebuffer_init(lcd);
+	if (ret != 0) {
+		printk("Failed to initialize framebuffer on %s: %d\n",
+		       lcd->name, ret);
+		return;
+	}
+
+	ret = cfb_framebuffer_clear(lcd, true);
+	if (ret != 0) {
+		printk("Failed to clear framebuffer on %s: %d\n", lcd->name,
+		       ret);
+		return;
+	}
+
+	ret = cfb_framebuffer_invert(lcd);
+	if (ret != 0) {
+		printk("Failed to invert framebuffer on %s: %d\n", lcd->name,
+		       ret);
+		return;
+	}
+
+	ret = cfb_print(lcd, "Press PRG", 0, 0);
+	if (ret != 0) {
+		printk("Failed to print string on %s: %d\n", lcd->name, ret);
+		return;
+	}
+
+	ret = cfb_framebuffer_finalize(lcd);
+	if (ret != 0) {
+		printk("Failed to finalize framebuffer on %s: %d\n", lcd->name,
+		       ret);
+		return;
+	}
+
+	ret = display_blanking_off(lcd);
+	if (ret != 0) {
+		printk("Failed to turn display blanking off on %s: %d\n",
+		       lcd->name, ret);
 		return;
 	}
 
